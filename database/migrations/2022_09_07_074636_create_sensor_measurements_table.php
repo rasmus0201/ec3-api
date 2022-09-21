91f1dd877036979b59dc85d874058080dd7190df
Rasmus Bundsgaard Sørensen <rso@ordbogen.com>
2022-09-21 13:41:43 +0200

    Fix column type

--- database/migrations/2022_09_07_074636_create_sensor_measurements_table.php
+++ database/migrations/2022_09_07_074636_create_sensor_measurements_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\{Device, Sensor};
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,8 +16,8 @@ return new class extends Migration
     {
         Schema::create('sensor_measurements', function (Blueprint $table) {
             $table->id();
-            $table->unsignedTinyInteger('device_id');
-            $table->unsignedTinyInteger('sensor_id');
+            $table->foreignIdFor(Device::class);
+            $table->foreignIdFor(Sensor::class);
             $table->double('value', 8, 2);
             $table->timestamp('measured_at', 6);
             $table->timestamp('created_at', 6)->nullable();
